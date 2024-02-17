095c33ca2cf1dd0625187f38142bc09ba9e3c951
hyckhee <husker4864@gmail.com>
2024-02-17 18:43:35 +0900

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set("port", process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, "/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/build/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 app.listen(app.get("port"), () => {
